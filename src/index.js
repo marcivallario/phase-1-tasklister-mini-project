4be66ee963b243077c9be9c14dd13c1ae4562dfc
Marci Vallario <marcivallario@gmail.com>
2021-12-01 14:55:36 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    let form = document.getElementById('create-task-form');
+    let input = document.getElementById('new-task-description');
+    let list = document.getElementById('tasks')
+    let li = document.createElement('li')
+    li.innerText = input.value;
+    list.append(li);
+    form.reset();
+  })
+})
+
+
+
